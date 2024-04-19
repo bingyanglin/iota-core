b8689a5ea928d8f42b9dc393cc5f943e1b9422df
muXxer <git@muxxer.de>
2024-04-19 18:19:08 +0200

    Fix dashboard metrics types

--- components/dashboard_metrics/types.go
+++ components/dashboard_metrics/types.go
@@ -2,26 +2,26 @@ package dashboardmetrics
 
 // NodeInfoExtended represents extended information about the node.
 type NodeInfoExtended struct {
-	Version       string `json:"version"`
-	LatestVersion string `json:"latestVersion"`
-	Uptime        int64  `json:"uptime"`
-	NodeID        string `json:"nodeId"`
-	NodeAlias     string `json:"nodeAlias"`
-	MemoryUsage   int64  `json:"memUsage"`
+	Version       string `serix:",lenPrefix=uint8"`
+	LatestVersion string `serix:",lenPrefix=uint8"`
+	Uptime        int64  `serix:""`
+	NodeID        string `serix:",lenPrefix=uint8"`
+	NodeAlias     string `serix:",lenPrefix=uint8"`
+	MemoryUsage   int64  `serix:""`
 }
 
 // DatabaseSizesMetric represents database size metrics.
 type DatabaseSizesMetric struct {
-	Permanent  int64 `json:"permanent"`
-	Prunable   int64 `json:"prunable"`
-	TxRetainer int64 `json:"txRetainer"`
-	Total      int64 `json:"total"`
-	Time       int64 `json:"ts"`
+	Permanent  int64 `serix:""`
+	Prunable   int64 `serix:""`
+	TxRetainer int64 `serix:""`
+	Total      int64 `serix:""`
+	Time       int64 `serix:""`
 }
 
 // GossipMetrics represents the metrics for blocks per second.
 type GossipMetrics struct {
-	Incoming uint32 `json:"incoming"`
-	New      uint32 `json:"new"`
-	Outgoing uint32 `json:"outgoing"`
+	Incoming uint32 `serix:""`
+	New      uint32 `serix:""`
+	Outgoing uint32 `serix:""`
 }
