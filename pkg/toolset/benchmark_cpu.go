521e90afa6ce051e6d332a6aa470e01e162cdcf7
muXxer <git@muxxer.de>
2024-05-08 15:13:12 +0200

    Remove unused const in tools

--- pkg/toolset/benchmark_cpu.go
+++ pkg/toolset/benchmark_cpu.go
@@ -2,15 +2,9 @@ package toolset
 
 import (
 	"context"
-	"crypto"
 	"sync/atomic"
 
 	"golang.org/x/crypto/blake2b"
-)
-
-const (
-	// Hash defines the hash function that is used to compute the PoW digest.
-	Hash = crypto.BLAKE2b_256
 )
 
 func cpuBenchmarkWorker(ctx context.Context, powDigest []byte, counter *uint64) {
