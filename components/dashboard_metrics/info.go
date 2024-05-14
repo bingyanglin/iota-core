0d7d03f8a05f8c998454171f916d68b1f5717ae4
muXxer <git@muxxer.de>
2024-05-14 15:32:54 +0200

    Add external MultiAddress to `NodeInfoExtended`

--- components/dashboard_metrics/info.go
+++ components/dashboard_metrics/info.go
@@ -13,12 +13,32 @@ func nodeInfoExtended() *NodeInfoExtended {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 
+	getExternalMultiAddr := func() string {
+		var fallback string
+
+		for i, addr := range deps.Host.Addrs() {
+			if i == 0 {
+				fallback = addr.String()
+			}
+
+			for _, protocol := range addr.Protocols() {
+				// search the first dns address
+				if protocol.Name == "dns" {
+					return addr.String()
+				}
+			}
+		}
+
+		return fallback
+	}
+
 	status := &NodeInfoExtended{
 		Version:       deps.AppInfo.Version,
 		LatestVersion: deps.AppInfo.LatestGitHubVersion,
 		Uptime:        time.Since(nodeStartupTimestamp).Milliseconds(),
 		NodeID:        deps.Host.ID().String(),
-		NodeAlias:     ParamsNode.Alias,
+		MultiAddress:  getExternalMultiAddr(),
+		Alias:         ParamsNode.Alias,
 		MemoryUsage:   int64(m.HeapAlloc + m.StackSys + m.MSpanSys + m.MCacheSys + m.BuckHashSys + m.GCSys + m.OtherSys),
 	}
 
